db67595336bd98ba3b5202110a2bfd8c78daf8db
Supercoder8888 <simonlqf@hotmail.com>
2023-10-07 14:18:37 +0800

    Build sort selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -3,10 +3,8 @@ import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/useGames";
 
-// 1.1)add onSelectePlatfrom function here
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  // 3.2)add selectedPlatform here
   selectedPlatform: Platform | null;
 }
 
@@ -17,7 +15,6 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
 
   return (
     <Menu>
-      {/* 3.3)Platform -> { .. }  */}
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
@@ -25,7 +22,6 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {data.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
-            // 1.2)with this we notify App components
             key={platform.id}
           >
             {platform.name}
