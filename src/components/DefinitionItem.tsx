1e64b0f6481a543d84f8db3f2fffe5fa006f3b3e
Supercoder8888 <simonlqf@hotmail.com>
2023-10-13 14:36:20 +0800

    Build game attributes

---
+++ src/components/DefinitionItem.tsx
@@ -0,0 +1,24 @@
+import { Box, Heading } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  term: string;
+  children: ReactNode | ReactNode[];
+}
+
+const DefinitionItem = ({ term, children }: Props) => {
+  return (
+    <Box marginY={5}>
+      {/* replace <dt> to chakra-ui ->  */}
+      <Heading as="dt" fontSize="md" color="gray.600">
+        {term}
+      </Heading>
+      <dd>{children}</dd>
+    </Box>
+  );
+};
+
+export default DefinitionItem;
+
+//'dt': HTML element, which short for definition term
+//'dd': short for definition description
