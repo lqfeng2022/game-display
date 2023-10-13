dd0a3b5aa32760e9c2ccbf3100e924af2c274474
Supercoder8888 <simonlqf@hotmail.com>
2023-10-13 17:35:33 +0800

    Improve the Layout and fix the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,19 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      {/* 1.1)add 'Link' component here */}
+      <Link to="/">
+        {/* 2.2)'objectFit': fix issue(image gets a little bit squash) */}
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
-    </HStack> 
+    </HStack>
   );
 };
 
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,27 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  //2.1)use Navigate hook to get the navigate f
+  const navigate = useNavigate();
+  
+  //2)ISSUE: on GameDetailPage, search for sth, nothing happen???
+  //->FIX the issue, we should tell the user back to the homepage,
+  //  cus that's where we show the games..
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        //2.2)when submit the form, we set SearchText, 
+        // then call navigate to take the user back to homepage
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup>
