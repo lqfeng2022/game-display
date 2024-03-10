a37ddefe82ca7b0d5f127d66b44b7c9ead538842
Supercoder8888 <simonlqf@hotmail.com>
2024-03-10 13:20:23 +0800

    Change Image to Text with 'GAME HUB' in 'NavBar' component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.webp";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack padding="10px">
-      {/* 1.1)add 'Link' component here */}
+    <HStack padding="10px" margin={2}>
       <Link to="/">
-        {/* 2.2)'objectFit': fix issue(image gets a little bit squash) */}
-        <Image src={logo} boxSize="60px" objectFit="cover" />
+        <Text as="i" whiteSpace="nowrap" fontSize="lg">
+          GAME HUB
+        </Text>
       </Link>
       <SearchInput />
       <Text color="gray.300" whiteSpace="nowrap" fontSize="md" as="i">
