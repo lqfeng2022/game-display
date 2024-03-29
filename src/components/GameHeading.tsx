1434328a999d57045f5f624d48c8f17119c137aa
lqfeng2022 <simonlqf@hotmail.com>
2024-03-29 15:22:18 +0800

    GameHeading font size change: '4xl' -> '3xl'

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -13,7 +13,7 @@ const GameHeading = () => {
 
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games `;
   return (
-    <Heading as="h1" marginY={5} fontSize="5xl">
+    <Heading as="h1" marginY={5} fontSize="3xl">
       {heading}
     </Heading>
   );
